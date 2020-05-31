eccb527c422aecced128590538b713772d788b8c
fredysolars <59477546+fredysolars@users.noreply.github.com>
2020-05-31 08:15:44 -0500

    Update webapp.js

--- webapp.js
+++ webapp.js
@@ -1,31 +1,16 @@
 
 const express = require('express')
 const aplicacion = express()
-
-
-
 const bodyParser = require('body-parser')
-const session = require('cookie-session')
+const session = require('express-session')
 const flash = require('express-flash')
 const fileUpload = require('express-fileupload')
-
-
-
-
-
+const path = require('path')
+const PORT = process.env.PORT || 5000
 const rutasMiddleware = require('./routes/middleware')
 const rutasPublicas = require('./routes/publicas')
 const rutasPrivadas = require('./routes/privadas')
 
-/*
-var pool = mysql.createPool({
-  connectionLimit: 20,
-  host: 'localhost',
-  user: 'root',
-  password: 'root23',
-  database: 'blog'
-})
-*/
 aplicacion.use(bodyParser.json())
 aplicacion.use(bodyParser.urlencoded({ extended: true }))
 aplicacion.set("view engine", "ejs")
@@ -34,199 +19,14 @@ aplicacion.use(flash())
 aplicacion.use(express.static('public'))
 aplicacion.use(fileUpload())
 
-/*
-aplicacion.use('/admin/', (peticion, respuesta, siguiente) => {
-  if (!peticion.session.usuario) {
-    peticion.flash('mensaje', 'Debe iniciar sesión')
-    respuesta.redirect("/inicio")
-  }
-  else {
-    siguiente()
-  }
-})
-
-aplicacion.get('/', function (peticion, respuesta) {
-  pool.getConnection(function(err, connection) {
-    const consulta = `
-      SELECT
-      titulo, resumen, fecha_hora, pseudonimo, voto
-      FROM publicaciones
-      INNER JOIN autor
-      ON publicaciones.autor_id = autor.id
-      ORDER BY fecha_hora DESC
-      LIMIT 5
-    `
-    connection.query(consulta, function (error, filas, campos) {
-      respuesta.render('index', { publicaciones: filas })
-    })
-    connection.release()
-  })
-})
-
-
-aplicacion.get('/registro', function (peticion, respuesta) {
-  respuesta.render('registro', { mensaje: peticion.flash('mensaje') })
-})
-
-aplicacion.post('/procesar_registro', function (peticion, respuesta) {
-  pool.getConnection(function (err, connection) {
-
-    const email = peticion.body.email.toLowerCase().trim()
-    const pseudonimo = peticion.body.pseudonimo.trim()
-    const contrasena = peticion.body.contrasena
-
-    const consultaEmail = `
-      SELECT *
-      FROM autor
-      WHERE email = ${connection.escape(email)}
-    `
-
-    connection.query(consultaEmail, function (error, filas, campos) {
-      if (filas.length > 0) {
-        peticion.flash('mensaje', 'Email duplicado')
-        respuesta.redirect('/registro')
-      }
-      else {
-
-        const consultaPseudonimo = `
-          SELECT *
-          FROM autor
-          WHERE pseudonimo = ${connection.escape(pseudonimo)}
-        `
-
-        connection.query(consultaPseudonimo, function (error, filas, campos) {
-          if (filas.length > 0) {
-            peticion.flash('mensaje', 'Pseudonimo duplicado')
-            respuesta.redirect('/registro')
-          }
-          else {
-
-            const consulta = `
-                                INSERT INTO
-                                autor
-                                (email, contrasena, pseudonimo)
-                                VALUES (
-                                  ${connection.escape(email)},
-                                  ${connection.escape(contrasena)},
-                                  ${connection.escape(pseudonimo)}
-                                )
-                              `
-            connection.query(consulta, function (error, filas, campos) {
-              peticion.flash('mensaje', 'Usuario registrado')
-              respuesta.redirect('/registro')
-            })
-          }
-        })
-      }
-    })
-    connection.release()
-  })
-})
-
-aplicacion.get('/inicio', function (peticion, respuesta) {
-  respuesta.render('inicio', { mensaje: peticion.flash('mensaje') })
-})
-
-
-aplicacion.post('/procesar_inicio', function (peticion, respuesta) {
-  pool.getConnection(function (err, connection) {
-    const consulta = `
-      SELECT *
-      FROM autor
-      WHERE
-      email = ${connection.escape(peticion.body.email)} AND
-      contrasena = ${connection.escape(peticion.body.contrasena)}
-    `
-    connection.query(consulta, function (error, filas, campos) {
-      if (filas.length > 0) {
-        peticion.session.usuario = filas[0]
-        respuesta.redirect('/admin/index')
-      }
-      else {
-        peticion.flash('mensaje', 'Datos inválidos')
-        respuesta.redirect('/inicio')
-      }
-
-    })
-    connection.release()
-  })
-})
-
-
-aplicacion.get('/admin/index', function (peticion, respuesta) {
-  pool.getConnection(function (err, connection) {
-    const consulta = `
-      SELECT *
-      FROM publicaciones
-      WHERE
-      autor_id = ${connection.escape(peticion.session.usuario.id)}
-    `
-    connection.query(consulta, function (error, filas, campos) {
-      respuesta.render('admin/index', { usuario: peticion.session.usuario, mensaje: peticion.flash('mensaje'), publicaciones: filas })
-    })
-    connection.release()
-  })
-})
-
-aplicacion.get('/procesar_cerrar_sesion', function (peticion, respuesta) {
-  peticion.session.destroy();
-  respuesta.redirect("/")
-});
-
-aplicacion.get('/admin/agregar', function (peticion, respuesta) {
-  respuesta.render('admin/agregar', { mensaje: peticion.flash('mensaje') , usuario: peticion.session.usuario})
-})
-
-aplicacion.post('/admin/procesar_agregar', function (peticion, respuesta) {
-  pool.getConnection(function (err, connection) {
-    const date = new Date()
-    const fecha = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
-    const consulta = `
-      INSERT INTO
-      publicaciones
-      (titulo, resumen, contenido, autor_id, fecha_hora)
-      VALUES
-      (
-        ${connection.escape(peticion.body.titulo)},
-        ${connection.escape(peticion.body.resumen)},
-        ${connection.escape(peticion.body.contenido)},
-        ${connection.escape(peticion.session.usuario.id)},
-        ${connection.escape(fecha)}
-      )
-    `
-    connection.query(consulta, function (error, filas, campos) {
-      peticion.flash('mensaje', 'Publicación agregada')
-      respuesta.redirect("/admin/index")
-    })
-    connection.release()
-  })
-})
-*/
-
-app.set('trust proxy', 1);
-
-app.use(session({
-cookie:{
-    secure: true,
-    maxAge:60000
-       },
-store: new RedisStore(),
-secret: 'secret',
-saveUninitialized: true,
-resave: false
-}));
-
-app.use(function(req,res,next){
-if(!req.session){
-    return next(new Error('Oh no')) //handle error
-}
-next() //otherwise continue
-});
 
 aplicacion.use(rutasMiddleware)
 aplicacion.use(rutasPublicas)
 aplicacion.use(rutasPrivadas)
 
-aplicacion.listen(5000, function(){
-  console.log("Servidor iniciado")
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Servidor iniciado on ${ PORT }`))
